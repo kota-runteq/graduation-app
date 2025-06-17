d64ecbd87f73f67efd238c77a69d775bcaaa4b99
kota-run-teq <sharp.vin3.14@gmail.com>
2025-06-17 12:05:41 +0900

    Add migration of menu table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_16_163627) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_17_024003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "menus", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_menus_on_name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
