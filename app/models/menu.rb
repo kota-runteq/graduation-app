3a5c51245e72c91c34065fdf095cb17a3f7f00c5
kota-run-teq <sharp.vin3.14@gmail.com>
2025-06-17 12:41:16 +0900

    Add association to model of menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,8 @@
 class Menu < ApplicationRecord
+  class Menu < ApplicationRecord
   validates :name, presence: true
+
+  has_many :menu_nutrients, dependent: :destroy
+  has_many :nutrients, through: :menu_nutrients
+  end
 end
